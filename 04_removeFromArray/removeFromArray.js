5d8428d3a6d118dcda4167c045cd6e3c5ec0bd8d
BROJ3 <toni.crnjak@hotmail.com>
2024-02-06 21:43:25 -0500

    starting exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    //we have an array and a value to remove, not a index
+
+    //loop through array
+    for (let i=0; i<array.length;i++) {
+
+        //loop through argumenst
+        for (let j=0; j<arguments.length;j++) {
+            // if for each element in array, one of the aguments that 
+            if (array[i]===arguments[j]) {
+                array.splice(i,1)
+            }
+            // isnt arg[0] aligns, remove it
+        }
+
+
+
+
+
+        
+    }
+    return array
+    //if something indexed matches valueToRemove, remove it
+ 
+    
+
+    
+
+    
 
 };
 

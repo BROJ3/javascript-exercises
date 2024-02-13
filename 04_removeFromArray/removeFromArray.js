532be6d39dc4475b2f731398f36151072e2b5a8c
BROJ3 <toni.crnjak@hotmail.com>
2024-02-12 22:41:27 -0500

    solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,15 +1,18 @@
 const removeFromArray = function (array) {
+
     //loop through arguments for each array piece
     for (let j = 1; j < arguments.length; j++) {
 
         //for each index of list check each argument
         for (let i = 0; i < array.length; i++) {
+
+
             // check if that argument is in the array 
             if (array[i] === arguments[j]) {
 
+                //remove all argument matches of array
+                array = array.filter(item => item !== arguments[j]);
 
-                //remove argument matches with indexed number
-                array.splice(i, 1)
             }
         }
     }

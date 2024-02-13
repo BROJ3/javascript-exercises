96974618d5d6ea7fb7b945d42259a1f3d42779bb
BROJ3 <toni.crnjak@hotmail.com>
2024-02-12 23:18:53 -0500

    solve most of exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function () {
 
+    if (arguments[0] > arguments[1]) {
+        //switch them
+        //put first argument in temporary variable
+        let temp = arguments[0];
+        arguments[0] = arguments [1];
+        arguments[1] = temp;
+    } 
+
+    stepsToGo = arguments[1] - arguments[0] + 1
+    stepCounter = 0
+
+    console.log("Steps to go: ", stepsToGo)
+
+    for (let i = 1; i < stepsToGo + 1; ++i) {
+        console.log(i)
+        stepCounter += i
+
+    }
+    console.log("stepcounter sayssss: ")
+    return stepCounter
 };
 
 // Do not edit below this line

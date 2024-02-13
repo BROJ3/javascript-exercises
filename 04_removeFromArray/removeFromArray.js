486bb07f43a1be6ed77c9c6979e86afe34d9f179
BROJ3 <toni.crnjak@hotmail.com>
2024-02-12 20:19:30 -0500

    progress on ex. 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,15 @@
-const removeFromArray = function(array) {
+const removeFromArray = function (array) {
+    //loop through arguments for each array piece
+    for (let j = 1; j < arguments.length; j++) {
 
-    //for each index of list check each argument
-    for (let i=0; i<array.length;i++) {
-
-        //loop through arguments for each array piece
-        for (let j=0; j<arguments.length;j++) {
-
+        //for each index of list check each argument
+        for (let i = 0; i < array.length; i++) {
             // check if that argument is in the array 
-            if (array[i]===arguments[j]) {
+            if (array[i] === arguments[j]) {
+
 
                 //remove argument matches with indexed number
-                array.splice(i,1)
+                array.splice(i, 1)
             }
         }
     }
